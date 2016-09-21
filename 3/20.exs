70901e029b622b74c82fa09b4856e0ce679960b9
Brett Gibson <bdg@brettdgibson.com>
2016-09-21 11:39:39 -0700

    Set 3 Ch 20.

--- 3/20.exs
+++ 3/20.exs
@@ -96,46 +96,11 @@ defmodule CryptUtils do
 
   def const_bytes(0, _), do: []
   def const_bytes(n, byte), do: (for _ <- 1..n, do: byte)
-end
-
-defmodule S3Ch19 do
-  @key CryptUtils.rand_bytes(16)
-  @nonce 0
-
-  @unsorted File.read!("3/19.txt")
-  |> String.split
-  |> Enum.map(&CryptUtils.parse_64/1)
-  |> Enum.map(&(CryptUtils.aes_ctr_encrypt(@key, @nonce, &1)))
-
-  @ctexts Enum.sort_by(@unsorted, &length/1) |> Enum.reverse
-  def ctexts, do: @ctexts
-
-  def guess_single_letter(letter), do: guess_single_letter(letter, ctexts)
-  def guess_single_letter(letter, offset) when is_number(offset) do
-    guess_single_letter(letter, Enum.drop(ctexts, offset))
-  end
-  def guess_single_letter(letter, [first | rest]) do
-    keystream_guess = Enum.map(first, &(letter ^^^ &1))
-    Enum.map(rest, &(CryptUtils.fixed_xor(keystream_guess, &1)))
-    |> columnize
-    |> likely_solves(keystream_guess)
-  end
-
-  def likely_solves(column_list, keystream_guess) do
-    column_list
-    |> Enum.with_index
-    |> Enum.reduce(%{}, fn({col, idx}, map) ->
-      if list_score(col) > 3.3 do
-        Map.put(map, idx, Enum.at(keystream_guess, idx))
-      else
-        map
-      end
-    end)
-  end
 
   def columnize([]), do: []
   def columnize(l) do
-    [heads(l) | columnize(tails(l))]
+    heads = heads(l)
+    if Enum.empty?(heads), do: [], else: [heads | columnize(tails(l))]
   end
   def tails([]), do: []
   def tails([[] | cdr]), do: tails(cdr)
@@ -147,62 +112,99 @@ defmodule S3Ch19 do
   def heads([car | cdr]) do
     [hd(car) | heads(cdr)]
   end
+end
 
-  def list_score([]), do: 0
-  def list_score(list) do
-    (list |> Enum.map(&char_score/1) |> Enum.sum) / length(list)
+defmodule S3Ch20 do
+  @key CryptUtils.rand_bytes(16)
+  @nonce 0
+
+  @ctexts File.read!("3/20.txt")
+  |> String.split
+  |> Enum.map(&CryptUtils.parse_64/1)
+  |> Enum.map(&(CryptUtils.aes_ctr_encrypt(@key, @nonce, &1)))
+
+  @min_len Enum.map(@ctexts, &length/1) |> Enum.min
+  @truncated Enum.map(@ctexts, &(Enum.slice(&1, 0, @min_len)))
+
+  @columns CryptUtils.columnize(@truncated)
+
+  def ctexts, do: @ctexts
+  def columns, do: @columns
+
+  def solve_columns do
+    [best_key_byte(hd(columns), &first_char_score/1) | solve_columns(tl(@columns))]
+  end
+  def solve_columns([]), do: []
+  def solve_columns([car | cdr]) do
+    [best_key_byte(car) |  solve_columns(cdr)]
+  end
+  def best_key_byte(col, scorer \\ &char_score/1) do
+    column_scores(col, scorer)
+    |> Enum.max_by(&(elem(&1, 1)))
+    |> elem(0)
+  end
+  def column_scores(col, scorer \\ &char_score/1) do
+    1..0xff
+    |> Enum.map(fn(n) ->
+      dec = CryptUtils.fixed_xor(col, CryptUtils.const_bytes(length(col), n))
+      {n, list_score(dec, scorer)}
+    end)
+    |> Enum.sort_by(&(-elem(&1, 1)))
+  end
+
+  def list_score([], _scorer), do: 0
+  def list_score(list, scorer) do
+    (list |> Enum.map(scorer) |> Enum.sum) / length(list)
+  end
+
+  def first_char_score(c) do
+    case c do
+      x when x in '\'"1' -> 5
+      x when x in 65..90 -> 5 # uppercase
+      x when x in 97..122 -> 2 # lowercase
+      x when x in 48..57 -> 1 # numbers
+      x when x in '.?,!;\'"' -> 1
+      x when x < 9 or x in 14..31 or x in 127..255 -> -50 # unprintable
+      _ -> -1
+    end
   end
   def char_score(c) do
     case c do
-      x when x in 'aeiou ' -> 5
-      x when x in 'dlprst\n' -> 3
+      x when x in ' etaoinshrdlu' -> 5
       x when x in 97..122 -> 2 # lowercase
-      x when x in 65..90 -> 1 # uppercase
+      x when x in 65..90 -> 2 # uppercase
       x when x in 48..57 -> 1 # numbers
-      x when x < 9 or x in 14..31 or x in 127..255 -> -50
+      x when x in '.?,!;\'"/' -> 1
+      x when x < 9 or x in 14..31 or x in 127..255 -> -50 # unprintable
       _ -> -1
     end
   end
 
-  def try_solves(solves) do
-    try_keystream for n <- 0..length(hd(ctexts)), do: solves[n] || 0
-  end
-
   def try_keystream(ks) do
-    for ctext <- @unsorted do
-      # IO.puts '012345679-12345679+12345679*12345679'
-      IO.puts CryptUtils.fixed_xor(ks, ctext)
+    for {ctext, idx} <- Enum.with_index(@ctexts) do
+      idx = idx |> Integer.to_string |> String.rjust(3, ?\s)
+      IO.puts "#{idx}: #{CryptUtils.fixed_xor(ks, ctext)}"
     end
-    nil
   end
 end
 
-# IO.inspect ['num ctexts', length(S3Ch19.ctexts)]
-# IO.inspect ['lengths', Enum.map(S3Ch19.ctexts, &length/1)]
+# col = Enum.at(S3Ch20.columns, 21)
+# IO.inspect col
+# scores = S3Ch20.column_scores(col)
+# for {b, s} <- Enum.slice(scores, 0, 10) do
+#   IO.inspect {b, s}
+#   IO.inspect CryptUtils.fixed_xor(col, CryptUtils.const_bytes(length(col), b))
+# end
 
-# My approach was going to be that of Ch 20 until I read it too early. So this is arbitrarily not
-# (precisely) that.
+keystream_guess = S3Ch20.solve_columns
+S3Ch20.try_keystream(keystream_guess)
 
-guesses = ' eaioustrlnbcdfghjklopqvwxyz?.!,;'
-solves = Enum.reduce guesses, %{}, fn(char, map) ->
-  Enum.reduce(1..30, %{}, fn(n, map) ->
-    Map.merge(S3Ch19.guess_single_letter(char, n), map)
-  end)
-  |> Map.merge(map)
-end
+# IO.inspect Enum.map(S3Ch20.ctexts, &length/1) |> Enum.with_index |> Enum.max_by(&(elem(&1, 0)))
 
-IO.inspect solves
-# %{11 => 160, 26 => 59, 15 => 248, 20 => 238, 17 => 47, 25 => 153, 13 => 233,
-#   0 => 11, 8 => 115, 7 => 88, 1 => 173, 32 => 24, 3 => 103, 6 => 226, 2 => 221,
-#   33 => 94, 10 => 3, 9 => 77, 19 => 33, 14 => 93, 5 => 87, 18 => 206, 31 => 235,
-#   22 => 224, 29 => 160, 21 => 107, 27 => 107, 24 => 240, 30 => 4, 23 => 208,
-#   16 => 251, 4 => 238, 12 => 121}
-S3Ch19.try_solves(solves)
-
-# ...some Googling...
 IO.puts ''
-S3Ch19.try_keystream CryptUtils.fixed_xor('He, too, has been changed in his turn,',
-                                          hd(S3Ch19.ctexts))
 
-
-
+# ... googling for lyrics to get untruncated part...
+googled_line = 'You want to hear some sounds that not only pounds but please your eardrums;' ++
+               ' / I sit back and observe the whole scenery'
+googled_keystream = CryptUtils.fixed_xor(Enum.at(S3Ch20.ctexts, 26), googled_line)
+S3Ch20.try_keystream(googled_keystream)
